3b79660642ce55831f32b1748c9df92709f9097a
Afrah333333 <107554936+Afrah333333@users.noreply.github.com>
2022-09-26 09:21:55 -0700

    first version test

---
+++ src/com/demogit/Main.java
@@ -0,0 +1,11 @@
+package com.demogit;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int celsius, fahrenheit;
+    fahrenheit = 59;
+    celsius = (fahrenheit - 32) * 5/9;
+        System.out.println(celsius);
+    }
+}
