578175f718f4f8ca4582e801b753180a8d1bee5d
Afrah333333 <107554936+Afrah333333@users.noreply.github.com>
2022-09-26 14:32:26 -0700

    a branch

--- src/com/demogit/Main.java
+++ src/com/demogit/Main.java
@@ -7,5 +7,8 @@ public class Main {
     fahrenheit = 59;
     celsius = (fahrenheit - 32) * 5/9;
         System.out.println(celsius);
+        //conveting to fahrenheit from celsius
+        fahrenheit = (celsius * 9/5) + 32;
+        System.out.println(fahrenheit);
     }
 }
